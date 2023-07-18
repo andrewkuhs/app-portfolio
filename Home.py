1988278f829803005b6d353c0003b0e167867ae7
Andrew Kuhs <andythekuhs@gmail.com>
2023-07-18 18:53:42 -0400

    second commit

---
+++ Home.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/andrew_photo.JPG")
+
+with col2:
+    st.title("Andrew Kuhs")
+    content = """
+    Hi, I'm Andy. I'm a math teacher and coding student.
+    """
+    st.write(content)
+
+content2 = """
+There are some apps I'm working on listed below.
+"""
+
+st.write(content2)
+
+df = pd.read_csv("data.csv", sep=";")
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
